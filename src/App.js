2450c88a12eeec8bccd3facdcdf66ed7f185830e
jkarnage <jakarnage@gmail.com>
2022-07-13 15:23:51 -0400

    Added rest of card components.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import "./App.css";
+import expData from "./data";
+
+const expCards = expData.map((exp) => {
+  return (
+    <Card
+      key={exp.id}
+      {...exp}
+    />
+  );
+});
 
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
-      <Card />
+      <section className="cards-list">{expCards}</section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,30 @@
-import profile from "../images/katie-zaferes.png";
 import star from "../images/star.png";
 
-export default function Card() {
+export default function Card(props) {
+  let badgeText =
+    props.openSpots === 0
+      ? "Sold Out"
+      : props.location === "Online"
+      ? "Online"
+      : null;
   return (
-    <section className="card">
-      <p className="card--status">SOLD OUT</p>
-      <img src={profile} alt="Experience profile" className="card--photo" />
-      <div className="card--info">
-        <div className="card--info--rating">
-          <img src={star} alt="star" className="card--info--rating--star" />
-          <span className="card--info--rating--value">5.0</span>
-          <span className="card--info--rating--quantity">(6)</span>
-          <span className="card--info--rating--location">•USA</span>
-        </div>
-        <p className="card--info--summary">Life lessons with Katie Zaferes</p>
-        <div className="card--info--pricing">
-          <span className="card--info--pricing--floor">From $136</span>
-          <span className="card--info--pricing--unit"> / person</span>
-        </div>
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={require(`../images/${props.coverImg}`)}
+        className="card--image"
+        alt="Experience profile"
+      />
+      <div className="card--stats">
+        <img src={star} className="card--star" alt="star" />
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
-    </section>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
   );
 }
